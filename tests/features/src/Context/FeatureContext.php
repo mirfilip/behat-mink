62c3b35767d7b4bb6ba17b50ff0bffe704452eed
Mirosław Filip <miroslaw.filip@schibsted.pl>
2016-10-21 10:21:27 +0200

    Setup project

---
+++ tests/features/src/Context/FeatureContext.php
@@ -0,0 +1,15 @@
+<?php
+namespace Behat\Context;
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Calculator\Calculator;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+}
